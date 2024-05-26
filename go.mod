bb6737ac1036c09d02fc309d535b27c861e4548d
Menno Finlay-Smits <hello@menno.io>
2024-05-26 22:21:16 +1200

    wip

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
-module example.com/gomod2nix-template
+module systemd-faild
 
-go 1.17
+go 1.22
+
+require (
+	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
+	github.com/godbus/dbus/v5 v5.0.4 // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"fmt"
+	"github.com/godbus/dbus/v5"
 )
 
+
+func notify() error {
+	conn, err := dbus.ConnectSessionBus()
+	if err != nil {
+		return err
+	}
+	obj := conn.Object("org.freedesktop.Notifications", "/org/freedesktop/Notifications")
+	call := obj.Call(
+		"org.freedesktop.Notifications.Notify",
+		0,
+		"systemd-mon",
+		uint32(0), // replaces id 
+		"", // app icon
+		"hello",
+		"there, world",
+		[]string{}, // actions
+		map[string]dbus.Variant{}, // hints
+		int32(5000),
+	)
+	if call.Err != nil {
+		return fmt.Errorf("error sending notification: %w", call.Err)
+	}
+	var ret uint32
+	err = call.Store(&ret)
+	if err != nil {
+		return fmt.Errorf("error getting uint32 ret value: %w", err)
+	}
+	return nil
+}
+
 func main() {
-	fmt.Println("Hello flake")
+	fmt.Println("Hello")
+	notify()
+	fmt.Println("Sent")
 }
